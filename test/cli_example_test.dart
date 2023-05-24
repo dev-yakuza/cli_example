5120a4acdb4aba22d9647b3bc0690c0f2ea6f28c
dev-yakuza <dev.yakuza@gmail.com>
2023-05-24 20:44:44 +0900

    test: Refactor test code

--- test/cli_example_test.dart
+++ test/cli_example_test.dart
@@ -4,24 +4,25 @@ import 'package:args/command_runner.dart';
 import 'package:cli_example/cli_example.dart';
 import 'package:test/test.dart';
 
-var log = [];
-
 void main() {
   final runner = CommandRunner('test', 'test')..addCommand(Echo());
 
-  setUp(() {
-    log = [];
-  });
-
-  test('Echo test message', runWithPrint(() async {
+  test('Echo test message', runWithPrint((logs) async {
     await runner.run(['echo', '--message', 'test message']);
-    expect(log, ['Message: test message']);
+    expect(logs, ['Message: test message']);
   }));
 }
 
-void Function() runWithPrint(void Function() testFn) => () {
+void Function() runWithPrint(void Function(List<String> logs) testFn) => () {
+      List<String> logs = [];
+
       var spec = ZoneSpecification(print: (_, __, ___, String msg) {
-        log.add(msg);
+        logs.add(msg);
       });
-      return Zone.current.fork(specification: spec).run<void>(testFn);
+
+      void callback() {
+        testFn(logs);
+      }
+
+      return Zone.current.fork(specification: spec).run<void>(callback);
     };
