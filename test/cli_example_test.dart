72812627f1f4dc60f121040d841c1cf9243d966a
dev-yakuza <dev.yakuza@gmail.com>
2023-05-25 16:48:11 +0900

    refactor: Use run_with_print package

--- test/cli_example_test.dart
+++ test/cli_example_test.dart
@@ -1,28 +1,15 @@
-import 'dart:async';
-
 import 'package:args/command_runner.dart';
 import 'package:cli_example/cli_example.dart';
+import 'package:run_with_print/run_with_print.dart';
 import 'package:test/test.dart';
 
 void main() {
   final runner = CommandRunner('test', 'test')..addCommand(Echo());
 
-  test('Echo test message', runWithPrint((logs) async {
-    await runner.run(['echo', '--message', 'test message']);
-    expect(logs, ['Message: test message']);
-  }));
+  test('Echo test message', () {
+    runWithPrint((logs) async {
+      await runner.run(['echo', '--message', 'test message']);
+      expect(logs, ['Message: test message']);
+    });
+  });
 }
-
-void Function() runWithPrint(void Function(List<String> logs) testFn) => () {
-      List<String> logs = [];
-
-      var spec = ZoneSpecification(print: (_, __, ___, String msg) {
-        logs.add(msg);
-      });
-
-      void callback() {
-        testFn(logs);
-      }
-
-      return Zone.current.fork(specification: spec).run<void>(callback);
-    };
