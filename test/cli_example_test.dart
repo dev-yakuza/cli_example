a9c3d331b8c1a5d9164b6f4bb0f0888e07025579
dev-yakuza <dev.yakuza@gmail.com>
2023-06-04 19:46:53 +0900

    test: Add async-await

--- test/cli_example_test.dart
+++ test/cli_example_test.dart
@@ -6,8 +6,8 @@ import 'package:test/test.dart';
 void main() {
   final runner = CommandRunner('test', 'test')..addCommand(Echo());
 
-  test('Echo test message', () {
-    runWithPrint((logs) async {
+  test('Echo test message', () async {
+    await runWithPrint((logs) async {
       await runner.run(['echo', '--message', 'test message']);
       expect(logs, ['Message: test message']);
     });
